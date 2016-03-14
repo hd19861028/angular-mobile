30ec76c6287bcf53c3cb3bdab38bca5823ff8334
hd19861028 <282642547@qq.com>
2016-03-14 12:10:50 +0800

    update readme

--- test/app.js
+++ test/app.js
@@ -1,15 +1,15 @@
 var angular = new angular_mobile();
 var info = {
-	user: {
-		name: "hu jin di",
-		title: 'qd',
-		desc: 'ms',
-		selected: "2",
-		age:5
-	},
-	users: [{name:'hjd', age:18},{name:'zj', age:16}],
-	testarray: ['a','b','c','d','e'],
-	src: 'https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=567500855,645854378&fm=58'
+    user: {
+        name: "hu jin di",
+        title: '系统架构师',
+        desc: '拥有8年web开发经验',
+        selected: "2",
+        age:29
+    },
+    users: [{name:'hjd', age:18},{name:'zj', age:16}],
+    testarray: ['a','b','c','d','e'],
+    src: 'https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=567500855,645854378&fm=58'
 }
 
 angular.set('info', info);
