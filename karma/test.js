ca77500730d50f6c6b3eb7fa9821953ec99d44bf
hd19861028 <282642547@qq.com>
2016-03-15 12:13:27 +0800

    update karma test

--- karma/test.js
+++ karma/test.js
@@ -25,23 +25,7 @@ describe("the tiny angular library", function() {
 		} catch (e) {
 			errorObj = e;
 		}
-
-		/*
-		    expect(x).toEqual(y); 当x和y相等时候通过
-		    expect(x).toBe(y); 当x和y是同一个对象时候通过
-		    expect(x).toMatch(pattern); x匹配pattern（字符串或正则表达式）时通过
-		    expect(x).toBeDefined(); x不是undefined时通过
-		    expect(x).toBeUndefined(); x 是 undefined时通过
-		    expect(x).toBeNull(); x是null时通过
-		    expect(x).toBeTruthy(); x和true等价时候通过
-		    expect(x).toBeFalsy(); x和false等价时候通过
-		    expect(x).toContain(y);x（数组或字符串）包含y时通过
-		    expect(x).toBeLessThan(y); x小于y时通过
-		    expect(x).toBeGreaterThan(y); x大于y时通过
-		    expect(function(){fn();}).toThrow(e); 函数fn抛出异常时候通过
-		    
-		    expect(x).not.toEqual(y); not取反
-		  */
+		
 		expect(errorObj).toBeNull();
 	});
 });
